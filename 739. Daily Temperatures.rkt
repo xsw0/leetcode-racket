a1f21261fe939a476e9a5402ffa178e9c8194962
xsw0 <xsw.mail.0@gmail.com>
2021-04-10 16:32:49 +0800

    Update 739. Daily Temperatures.rkt

--- 739. Daily Temperatures.rkt
+++ 739. Daily Temperatures.rkt
@@ -9,4 +9,25 @@
                      (iterator (cdr l) (cons (car l) rl) (+ n 1))))
                (iterator T '() 0))
              T)])
-    '()))
+    (define (f result rt pair-day-tem current-day)
+      (if (empty? rt)
+          result
+          (let ([ele (car rt)])
+            (cond [(empty? pair-day-tem)
+                   (f (cons 0 result)
+                      (cdr rt)
+                      (list (cons current-day ele))
+                      (- current-day 1))]
+                  [(> (cdar pair-day-tem) ele)
+                   (f (cons (- (caar pair-day-tem) current-day) result)
+                      (cdr rt)
+                      (cons (cons current-day ele) pair-day-tem)
+                      (- current-day 1))]
+                  [else
+                   (f result
+                      rt
+                      (cdr pair-day-tem)
+                      current-day)]))))
+    (f '() (car rt) '() (cdr rt))))
+
+(daily-temperatures '(73 74 75 71 69 72 76 73))
